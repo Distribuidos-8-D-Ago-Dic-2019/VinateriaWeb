fbdb9dcb5a82307373eda4afa2534509059b826a
Eduardo Palomera <lalox1365@gmail.com>
2019-12-09 09:52:24 -0600

    a

--- modelo/ModeloProducto.php
+++ modelo/ModeloProducto.php
@@ -37,7 +37,15 @@
 		if (isset($respuesta->return)) {
 			$productos = $respuesta->return;
 		}else {
-			$productos = '';
+			$productos = array();
+			$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where nombre like '".$busqueda."%' and cantidad>0";
+			if($result = mysqli_query($this->conexion,$sql)){
+				while ($obj = mysqli_fetch_array($result)){
+					array_push(	$productos, $obj);
+				}
+				mysqli_free_result($result);
+			}
+			$this->conexion->close();
 		}
 		/*$productos = array();
 		$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where nombre like '".$busqueda."%' and cantidad>0";
