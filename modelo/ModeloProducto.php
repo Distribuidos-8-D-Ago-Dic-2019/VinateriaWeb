2f5c602f2974cb35c74bf4f7a4677f4146e2bd9c
Eduardo Palomera <lalox1365@gmail.com>
2019-12-09 09:53:33 -0600

    Update ModeloProducto.php

--- modelo/ModeloProducto.php
+++ modelo/ModeloProducto.php
@@ -9,8 +9,20 @@
 
 	function getProducto($id) {
 		$respuesta = $this->jax->getProductobyId(array('id' => $id));
-		$producto = $respuesta->return;
-		return $producto;
+		if (isset($respuesta->return)) {
+			$productos = $respuesta->return;
+		}else {
+			$productos = array();
+			$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where nombre like '".$busqueda."%' and cantidad>0";
+			if($result = mysqli_query($this->conexion,$sql)){
+				while ($obj = mysqli_fetch_array($result)){
+					array_push(	$productos, $obj);
+				}
+				mysqli_free_result($result);
+			}
+			$this->conexion->close();
+		}
+		return $productos;
 		/*$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where id= '".$id."'";
 		$result = $this->conexion->query($sql)->fetch_object();
 		$this->conexion->close();
@@ -19,7 +31,19 @@
 
 	function getProductos() {
 		$respuesta = $this->jax->getAllProductos();
-		$productos = $respuesta->return;
+		if (isset($respuesta->return)) {
+			$productos = $respuesta->return;
+		}else {
+			$productos = array();
+			$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where nombre like '".$busqueda."%' and cantidad>0";
+			if($result = mysqli_query($this->conexion,$sql)){
+				while ($obj = mysqli_fetch_array($result)){
+					array_push(	$productos, $obj);
+				}
+				mysqli_free_result($result);
+			}
+			$this->conexion->close();
+		}
 		/*$sql = "SELECT id, nombre, descripcion, precio, contenido, imagen, cantidad FROM producto where cantidad>0";
 		if($result = mysqli_query($this->conexion,$sql)){
 			while ($obj =  mysqli_fetch_array($result)){
